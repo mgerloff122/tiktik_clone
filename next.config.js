d4d964d88f7abfd521f3b2d6da31568ba7a4632e
Matthew Gerloff <matthewgerloff@Hsin-Lings-MBP.fuel.digital>
2022-07-26 12:21:50 -0400

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['i.picsum.photos', 
+  'lh3.googleusercontent.com'],
+  }
 }
 
 module.exports = nextConfig
